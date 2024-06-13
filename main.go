52b75d0d2402720442845ee8da6c0100970fc67b
K0max <zwliao0722@outlook.com>
2024-06-13 10:19:35 +0800

    add groupcontrol

--- main.go
+++ main.go
@@ -26,24 +26,50 @@ func main() {
 	// r.Run(":9999")
 
 	// day 3
+	// r := tinygin.New()
+	// r.GET("/", func(c *tinygin.Context) {
+	// 	c.HTML(http.StatusOK, "<h1>Hello tinygin</h1>")
+	// })
+	//
+	// r.GET("/hello", func(c *tinygin.Context) {
+	// 	// expect /hello?name=tinyginktutu
+	// 	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	// })
+	//
+	// r.GET("/hello/:name", func(c *tinygin.Context) {
+	// 	// expect /hello/tinyginktutu
+	// 	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	// })
+	//
+	// r.GET("/assets/*filepath", func(c *tinygin.Context) {
+	// 	c.JSON(http.StatusOK, tinygin.H{"filepath": c.Param("filepath")})
+	// })
+	//
+	// r.Run(":9999")
+
+	// day 4
 	r := tinygin.New()
 	r.GET("/", func(c *tinygin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello tinygin</h1>")
+		c.HTML(http.StatusOK, "<h1>index</h1>")
+	})
+	v1 := r.Group("/v1")
+	v1.GET("/", func(c *tinygin.Context) {
+		c.HTML(http.StatusOK, "<h1>htllo</h1>")
+	})
+	v1.GET("/hello", func(ctx *tinygin.Context) {
+		ctx.String(http.StatusOK, "Hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
 	})
 
-	r.GET("/hello", func(c *tinygin.Context) {
-		// expect /hello?name=tinyginktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	v2 := r.Group("/v2")
+	v2.GET("/hello/:name", func(ctx *tinygin.Context) {
+		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
 	})
 
-	r.GET("/hello/:name", func(c *tinygin.Context) {
-		// expect /hello/tinyginktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	v2.POST("/login", func(ctx *tinygin.Context) {
+		ctx.JSON(http.StatusOK, tinygin.H{
+			"username": ctx.PostForm("username"),
+			"password": ctx.PostForm("password"),
+		})
 	})
-
-	r.GET("/assets/*filepath", func(c *tinygin.Context) {
-		c.JSON(http.StatusOK, tinygin.H{"filepath": c.Param("filepath")})
-	})
-
 	r.Run(":9999")
 }
--- tinygin/tinygin.go
+++ tinygin/tinygin.go
@@ -1,6 +1,7 @@
 package tinygin
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,6 +10,7 @@ type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
+	*RouterGroup
 	router *router
 	groups []*RouterGroup
 }
@@ -16,13 +18,15 @@ type Engine struct {
 type RouterGroup struct {
 	prefix      string
 	middlewares []HandlerFunc
-	parent      *RouterGroup
 	engine      *Engine
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
@@ -47,4 +51,28 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handler(c)
+}
+
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (g *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	g.engine.router.addRoute(method, pattern, handler)
+	log.Printf("Route %4s - %s", method, pattern)
+}
+
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
 }
